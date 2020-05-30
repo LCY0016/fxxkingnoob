2bf8c704a17a5feafe1c94538aa6880ae1b377e0
LCY0016 <61568064+LCY0016@users.noreply.github.com>
2020-05-30 16:29:24 +0800

    Add files via upload

---
+++ 1/1.1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, c = 0;
+            Console.WriteLine("简易计算器");
+            Console.WriteLine("请输入第一个操作数：");
+
+            while (true) {
+                try
+                {
+                    a = Double.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("出现错误，请重新输入第一个操作数：");
+                    continue;
+                }
+                break;
+            }
+            
+
+            Console.WriteLine("请输入操作符：");
+            String b = Console.ReadLine();
+            Console.WriteLine("请输入第二个操作数：");
+            while (true)
+            {
+                try
+                {
+                    c = Double.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("出现错误，请重新输入第二个操作数：");
+                    continue;
+                }
+                break;
+            }
+            switch (b)
+            {
+                case "+": 
+                    Console.WriteLine("结果为:{0}",a+c); 
+                    break;
+                case "-": 
+                    Console.WriteLine("结果为：{0}",a-c); 
+                    break;
+                case "*": 
+                    Console.WriteLine("结果为：{0}",a*c); 
+                    break;
+                case "/":
+                    if (c == 0)
+                    {
+                        Console.WriteLine("除数不能为0");
+                    }
+                    else {
+                        Console.WriteLine("结果为：{0}", a/c);
+                    }
+                    break;
+                default:
+                    Console.WriteLine("请输入正确的操作符");
+                    break;
+            }
+        }
+    }
+}
